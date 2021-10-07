b95a56fedf908fd6495fed8f7e67f74a263e3c74
BrayanAbraham <brayanabraham@gmail.com>
2021-10-07 10:34:32 +0530

    Cleanup

--- src/helper/cmd.js
+++ src/helper/cmd.js
@@ -1,22 +1,6 @@
 const fs = require("fs");
 const Repository = require("../classes/Repository");
 
-const rootPathMode = () => {
-  const repository = new Repository(process.argv[3]);
-
-  // entryFile = packagedata.main;
-  // console.log(entryFile);
-  // try {
-  //   entryFileContents = fs.readFileSync(`${rootPath}/${entryFile}`, "utf-8");
-  // } catch (error) {
-  //   if (error.code === "ENOENT") {
-  //     error.message = "Index File not found";
-  //   }
-  //   throw error;
-  // }
-  // console.log(entryFileContents);
-};
-
 const cmd = () => {
   const mode = process.argv[2];
   let rootPath;
@@ -25,7 +9,7 @@ const cmd = () => {
   let entryFileContents;
   switch (mode) {
     case "-r":
-      rootPathMode();
+      const repository = new Repository(process.argv[3]);
       break;
     case "-e":
       rootPath = process.argv[3];
